05bd7c61b503041b0382dc12be65ffb7bac0c6a4
Agus-Coder <93748858+Agus-Coder@users.noreply.github.com>
2022-04-10 14:23:49 -0300

    Se termina ejercicio callbackHell

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # fundamentos_nodejs
+
+El archivo callbackHell representa un ejercicio bastante importante.
+Cada funcion tiene un callback y al callback lo podes entender como "Lo que tenes que hacer despues". Es decir, la funcion va a hacer algo y despues ejecuta su callback,
+(por lo menos en este ejercicio).
+
+Evitamos entrar en un hell porque declaramos una funcion que repite la cantidad de veces que queremos el "bla bla bla"
--- async/callbackHell.js
+++ async/callbackHell.js
@@ -5,27 +5,63 @@ function hola(nombre, miCallback) {
     }, 1500)
 }
 
+function hablar(callbackHablar){
+    setTimeout(function(){
+        console.log('bla bla bla bla bla...')
+        callbackHablar()
+    },1000)
+}
+
+// la siguiente es una funcion recursiva, que se repite cierta cantidad de veces
+function conversacion(nombre, veces, callback){
+    if (veces>0){
+    hablar(function(){
+        conversacion(nombre, --veces, callback)
+    })
+    } else{
+        callback(nombre, function(){
+            console.log('Proceso Terminado...')
+        })
+    }
+}
+
 function adios(nombre, callback2) {
     setTimeout(function () {
         console.log('Adios ' + nombre + '!')
         callback2()
     }, 1000)
-
 }
 
+//Es una buena practica haber escrito las funciones antes del codigo principal de ejecucion
+
+// --
+
+// Version 2: Fijate que la funcion es mucho mas limpia que antes
 console.log('inicia el proceso...')
-
-//Completamente asincrono
-
-hola("Agus", function (nombre) {
-    adios('Agus', function () {
-        console.log('Termina el proceso...')
-    })
+hola('Agus', function(nombre){
+    conversacion(nombre, 3, adios)
 })
 
-// sin asincronia
 
-// hola("Agus", function () {})
-// adios("Agus", function () {})
 
-// console.log('Termina el proceso...')
+//Version 1: antes tenias esto
+
+// hola("Agus", function (nombre) {
+//     hablar(function(){
+//         hablar(function(){
+//             hablar(function(){
+//                 adios('Agus', function () {
+//                     console.log('Termina el proceso...')
+//                 })
+//             })
+//         })
+//     })
+    
+// })
+
+
+// cuando trabajamos con callbacks mas complejos debemos proceder de otra manera.
+
+// todo el código anterior tiene etapas muy repetidas y tanta repeticion nos haria entrar en un callbackhell
+//debemos ser ingeniosos en como evitamos entrar en este infierno. para este caso puntual lo correcto seria crear otra funcion
+//que repita la conversacion las veces que sea necesaria para nueustra funcion principal
