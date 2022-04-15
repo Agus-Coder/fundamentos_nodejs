1a0e7cca40e8028253002fff7b98a8f2c72fd0e4
Agus-Coder <93748858+Agus-Coder@users.noreply.github.com>
2022-04-15 18:10:24 -0300

    se termina ejercicio de modulos en node.js

---
+++ paquetes/modulo/es6/index.mjs
@@ -0,0 +1,7 @@
+import modulo from './modulo.mjs'
+
+console.log(modulo.prop1)
+modulo.saludar()
+
+
+//Estoy usando una version mas nueva de Node en comparacion con el curso, los modulos de ECMA ya no son experimentales
